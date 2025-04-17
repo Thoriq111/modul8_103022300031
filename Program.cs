0dda4e7fb01a26c31f865d6dad4bb266c2197b8b
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 11:12:25 +0700

    Aplikasi bank transfer dengan runtime configuration

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Numerics;
+using Modul8_103022300031;
+using static Modul8_103022300031.BankTransferConfig;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        if (Config config lang == "en")
+        {
+            Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman? ");
+            int hariDemam = int.Parse(Console.ReadLine());
+        }
+    }
+}
+
+
+
+
+
+
